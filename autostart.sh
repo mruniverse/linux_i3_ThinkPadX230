3331717580eff55684848095fb3a7927faaa3c97
MrUniverse <yurisannin@hotmail.com>
2019-02-18 10:51:52 -0300

    Adding Media keys

--- autostart.sh
+++ autostart.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo '97' > /sys/devices/platform/i8042/serio1/serio2/speed
+echo '110' > /sys/devices/platform/i8042/serio1/serio2/sensitivity 
 pulseaudio --k
 sleep 1
 pulseaudio --start
