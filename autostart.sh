ddbdcaa296f3861ddf75549e534380a58c7d373f
mruniverse <yurisannin@hotmail.com>
2019-04-09 11:03:26 -0300

    att

--- autostart.sh
+++ autostart.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-echo '97' > /sys/devices/platform/i8042/serio1/serio2/speed
-echo '110' > /sys/devices/platform/i8042/serio1/serio2/sensitivity 
-pulseaudio --k
-sleep 1
-pulseaudio --start
+echo '90' > /sys/devices/platform/i8042/serio1/serio2/speed
+echo '120' > /sys/devices/platform/i8042/serio1/serio2/sensitivity 
